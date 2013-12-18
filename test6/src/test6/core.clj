6a0fa815e9b108cfbe1c2ed0b4ad9b525dc38eac
Claude Falguiere <cfalguiere@gmail.com>
2013-12-18 12:28:08 +0100

    now pass parameters

--- test6/src/test6/core.clj
+++ test6/src/test6/core.clj
@@ -3,8 +3,7 @@
 (defn finder [dbname key] (str "value from db " dbname)) 
 (defn pretty-printer [finder key] (str "This is a " (finder key)))
 
-(defn -main [& args]
-  (let [dbname "DBProd"
-	printer (partial pretty-printer (partial finder dbname))]  
+(defn -main [dbname]
+  (let [printer (partial pretty-printer (partial finder dbname))]  
    (printer "Alice") ))
  

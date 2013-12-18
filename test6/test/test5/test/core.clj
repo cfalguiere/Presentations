049c672a1fec077557e1a68d5a49e31c30eb98c0
Claude Falguiere <cfalguiere@gmail.com>
2013-12-18 12:21:04 +0100

    fixed folder name

--- test6/test/test5/test/core.clj
+++
@@ -1,9 +0,0 @@
-(ns test6.test.core
-  (:use [test5.core])
-  (:use [midje.sweet]))
-
-(defn fake-printer [key] "fake value")
-     
-(fact "it should display This is a"
-      (pretty-printer fake-printer "Bob") => "This is a fake value")
-
